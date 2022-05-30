2f4745aff6f0f0d3be6bfc835fb7ac040c483847
codelrond <codelrond@protonmail.com>
2022-05-30 19:17:47 +0200

    return serde an ndarray of two dimensions

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,25 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
+#[macro_use]
+extern crate ndarray;
+
+use ndarray::prelude::*;
 
 #[derive(Serialize, Deserialize)]
 pub struct Example {
     pub field1: HashMap<u32, String>,
-    pub field2: Vec<Vec<f32>>,
-    pub field3: [f32; 4],
+    pub field2: Vec<Vec<f32>>
+    //pub field3: [f32; 4],
+    //pub field3: Array2::<f64>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ResponseExample {
+    pub field1: HashMap<u32, String>,
+    pub field2: Array2::<f64>
+    //pub field3: [f32; 4],
+    //pub field3: Array2::<f64>
 }
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -31,12 +44,14 @@ pub fn greet() {
 pub fn multiply(val: &JsValue) -> JsValue{
     let arg: Example = val.into_serde().unwrap();
 
+    let mut arr = Array2::<f64>::zeros((2,2));
+    arr[ [0, 0] ] = -1.1;
+
     let mut field1 = HashMap::new();
-    field1.insert(0, String::from("ex"));
-    let example = Example {
+    field1.insert(0, String::from("ok! ;)"));
+    let example = ResponseExample {
         field1,
-        field2: arg.field2,
-        field3: [1., 2., 3., 4.]
+        field2: arr//arg.field2
     };
 
     JsValue::from_serde(&example).unwrap()
